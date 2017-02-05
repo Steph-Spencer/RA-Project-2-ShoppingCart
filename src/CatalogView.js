aad828be2dc35b759f5fbad88b20697b386a88da
Steph-Spencer <stephanie.e.spencer@gmail.com>
2017-02-05 17:23:09 -0500

    #006 Banner
    Completed banner. Also reverted to earlier version of ShoppingCart.js
    after I broke it beyond repair. Committing changes before next attempt.

--- src/CatalogView.js
+++ src/CatalogView.js
@@ -58,10 +58,11 @@ export default class CatalogView{
             // create a new IMG tag. Suggest to add data-sku attribute here too
             // so that if you 'click' on the image, it would pop up a quick-view
             // window and you can use the sku.
-            let newImg = document.createElement("img");
+            let newImg = document.createElement("img");//change to div
             newImg.setAttribute("src", product.image);
             newImg.setAttribute("alt", `${product.name}`); // this works too
             newImg.setAttribute("data-sku",product.sku);
+            // create a set attribute "style" `
 
             let hr = document.createElement("hr");
 
--- src/js/QuickView.js
+++ src/js/QuickView.js
@@ -8,7 +8,7 @@ export default class QuickView{
 
     }
     
-  initQuickView(){
+    initQuickView(){
      
     $(document).on('click', '#quickViewButton',function(evt){
        		console.log("Let the preview begin!");
@@ -16,10 +16,10 @@ export default class QuickView{
         	$(".overlay").toggle();         
     });
        
-       $(document).on('click', '#close, .overlay',function(evt){
+      /* $(document).on('click', '#close, .overlay',function(evt){
        		console.log("Give me the quickview!");
         	$("#quickview").toggle(); 
         	$(".overlay").toggle();         
-    	});
+    	}); */
   }
 }
